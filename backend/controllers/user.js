6de79a175d13ae4a390f6a8050e2385c59f8bb45
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-03-26 15:14:51 +0100

    imrpoved user authentification and signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,48 +1,44 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-exports.signup = (req, res, next) => {
-    bcrypt
-        .hash(req.body.password, 10)
-        .then((hash) => {
-            const user = new User({
-                email: req.body.email,
-                password: hash,
-            });
-            user.save()
-                .then(() =>
-                    res.status(201).json({ message: "Utilisateur créé !" })
-                )
-                .catch((error) => res.status(400).json({ error }));
-        })
-        .catch((error) => res.status(500).json({ error }));
+exports.signup = async (req, res, next) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            email: req.body.email,
+            password: hashedPassword,
+        });
+        await user.save();
+        res.status(201).json({ message: "Utilisateur créé !" });
+    } catch (error) {
+        res.status(500).json({ error: "Une erreur est survenue." });
+    }
 };
 
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then((user) => {
-            if (!user) {
-                return res
-                    .status(401)
-                    .json({ error: "Utilisateur non trouvé !" });
-            }
-            bcrypt
-                .compare(req.body.password, user.password)
-                .then((valid) => {
-                    if (!valid) {
-                        return res
-                            .status(401)
-                            .json({ error: "Mot de passe incorrect !" });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            "RANDOM_TOKEN_SECRET",
-                            { expiresIn: "24h" }
-                        ),
-                    });
-                })
-                .catch((error) => res.status(500).json({ error }));
-        })
-        .catch((error) => res.status(500).json({ error }));
+exports.login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(401).json({ error: "Utilisateur non trouvé !" });
+        }
+        const passwordMatch = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!passwordMatch) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+        }
+        const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "24h",
+        });
+        res.status(200).json({
+            userId: user._id,
+            token: token,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Une erreur est survenue lors de l'authentification.",
+        });
+    }
 };
